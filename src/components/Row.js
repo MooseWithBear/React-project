1aed76a15696a9c7da920817939c4c03b120a596
MooseWithBear <moosewithbear@gmail.com>
2022-12-23 16:58:53 +0900

    [react] add components (Row, Category)

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,68 @@
 // rafce snippet
 
-import React from 'react'
+import axios from "../api/axios";
+import React, { useCallback, useEffect, useState } from "react";
+import "./Row.css";
+import MovieModal from "./MovieModal";
 
-const Row = () => {
+const Row = ({ title, id, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({})
+
+  const fetchMovieData = useCallback(async () => {
+    const response = await axios.get(fetchUrl);
+    console.log("response: ", response);
+    setMovies(response.data.results);
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchMovieData]);
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie)
+  };
+
   return (
-    <div>Row</div>
-  )
-}
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
+        </div>
 
-export default Row
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img key={movie.id} className="row__poster" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.name} onclick={() => {}} />
+          ))}
+        </div>
+
+        <div className="slider__arrow-right">
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
+        </div>
+      </div>
+      {setModalOpen && 
+        <MovieModal 
+        {...movieSelected}
+        setModalOpen={setModalOpen} />}
+    </div>
+  );
+};
+
+export default Row;
